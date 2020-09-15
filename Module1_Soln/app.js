f6e493dd0c8d25cfa1555e035a6bd11ed2728e58
Neha Mishra <neha79091@gmail.com>
2020-09-15 22:54:10 +0530

    LunchCheckAppAdded

---
+++ Module1_Soln/app.js
@@ -0,0 +1,42 @@
+(function()
+{
+  'use strict';
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController',LunchCheckController);
+  function LunchCheckController($scope,$filter)
+  {
+  $scope.LunchList="ik,do,tin";
+
+
+  $scope.checkLunch=function()
+    {
+      if($scope.LunchList == ""){
+        $scope.count = 0;
+      }
+      else{
+
+        $scope.count=$scope.LunchList.split(',').length;
+      }
+      $scope.sayMessage="";
+
+
+
+
+        if($scope.count == 0)
+        {
+          $scope.sayMessage= "Please Enter Data First";
+        }
+        else if($scope.count<=3)
+        {
+          $scope.sayMessage= "Enjoy";
+        }
+        else
+        {$scope.sayMessage= "Too much";}
+
+
+    };
+
+  }
+
+
+})();
